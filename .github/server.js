6c356e4aa59e21ac3b2206b70708b8784f5f2675
Kaan Nyman <nymanka@ad.unc.edu>
2022-02-17 22:58:04 -0500

    oof

--- .github/server.js
+++ .github/server.js
@@ -1,3 +1,16 @@
+//require dependencies
+import * as coin from "../a02-nymanka/modules/coin.mjs"
+import express from 'express';
+import Yargs from 'yargs';
+
+const app = express();
+const argv = Yargs(process.argv.slice(2)).argv;
+
+//define port as default or from cli
+const port = (argv.port == undefined) ? 5000 : argv.port;
+
+
+
 // Require Express.js
 const express = require('express')
 const app = express()
@@ -21,17 +34,27 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
-app.get('/app/flips/:number', (req, res) => {
-    //Some
-    //expressions
-    //go
-    //here
-    });
+
+//random coin flip endpoint
+app.get('/app/flip', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json({ 'flip' : coin.coinFlip()});
+});
 
 app.get('/app/flips/:number', (req, res) => {
-    const flips = manyflips(req.params.number)
-    //Other
-    //expressions
-    //go
-    //here
-    });
+    res.contentType('text/json');
+    const flips = coin.coinFlips(req.params.number);
+    const count = coin.countFlips(flips);
+    res.status(200).json({'Flips': flips,'info': count});
+});
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json(coin.coinFlip('heads'));
+});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.contentType('text/json');
+    res.status(200).json(coin.coinFlip('tails'));
+});
+
