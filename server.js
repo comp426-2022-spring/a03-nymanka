52e18448b50fe9dc0a429990e8b362cbdb6b30e5
Kaan Nyman <nymanka@ad.unc.edu>
2022-03-02 20:59:10 -0500

    ff

--- server.js
+++ server.js
@@ -2,84 +2,115 @@
 const express = require('express')
 const app = express()
 const args = require('minimist')(process.argv.slice(2))
+
 args['port']
+
 const call = args.call
 const port = args.port || process.env.PORT || 5000
 
+
 // Start an app server
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',port))
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
+
+// Define check endpoint
 app.get('/app/', (req, res) => {
     // Respond with status 200
-	res.statusCode = 200;
-	// Respond with status message "OK"
-		res.statusMessage = 'OK';
-		res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-		res.end(res.statusCode+ ' ' +res.statusMessage)
+    res.statusCode = 200;
+
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage);
 });
 
 
 function coinFlip() {
-	return Math.random() > 0.5 ? ("heads") : ("tails")
-}
+    return (Math.random() > 0.5 ? "heads" : "tails");
+  }
 
-app.get('/app/flip', (req, res) => {
-	var flip = coinFlip()
-	res.status(200).json({ 'flip' : flip})
-})
+
+app.get('/app/flip/', (req, res) => {
+    res.send({ flip: coinFlip() });
+});
+
 
 function coinFlips(flips) {
-	const arr = []
-	for(let i = 1; i <= flips; i++) {
-	  arr.push(Math.random() > 0.5 ? ("heads") : ("tails"))
-	}
-	return arr
-}
+    const coin = [];
+  
+    for(let i = 0; i < flips; i++){
+      Math.random() > 0.5 ? coin.push("heads") : coin.push("tails");
+    }
+  
+    const output = { raw: [], summary: "" };
+    output.raw = coin;
+    output.summary = countFlips(coin);
+    return output
+  }
 
 function countFlips(array) {
-	const Tab = {tails: 0, heads: 0}
-	for(let i = 0; i < array.length; i++) {
-	  if(array[i] == "heads"){
-		Tab.heads = Tab.heads + 1
-	  } else {
-		Tab.tails = Tab.tails + 1
-	  }
-	}
-	if(Tab.heads == 0){
-	  delete Tab.heads
-	} if(Tab.tails ==0){
-	  delete Tab.tails
-	}
-	return(Tab)
-}
+    var heads = 0;
+    var tails = 0;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] == 'heads'){
+        heads++;
+      }
+      else{
+        tails++;
+      }
+    }
+    const count = new Map();
+  
+    count.set("tails", tails);
+    count.set("heads", heads);
+    
+    return count;
+  
+  }
+
+
 app.get('/app/flips/:number', (req, res) => {
-	const flips = coinFlips(req.params.number)
-	res.status(200).json({ 'raw': flips, "summary" : countFlips(flips)})
+    res.send(coinFlips(req.params.number));
 });
+
 
 function flipACoin(call) {
-	const Tab = {call: "", flip: "", result: ""}
-	Tab.call = call
-	Tab.flip = Math.random() > 0.5 ? ("heads") : ("tails")
-	if(Tab.call == Tab.flip){
-	  Tab.result = "win"
-	} else {
-	  Tab.result = "lose"
-	}
-	return Tab
+    if (call != "heads" & call != "tails" | call == null) {
+      console.log("Error: no input.")
+      console.log("Usage: node guess-flip --call=[heads|tails]")
+    }else{
+      const flip = coinFlip();
+      var result = "lose";
+      if(call == flip){
+        result = "win";
+    }
+    const count = new Map();
+  
+    count.set("call", call);
+    count.set("flip", flip);
+    count.set("result", result);
+   
+    return count;
+  
+    }
 }
 
+
 app.get('/app/flip/call/heads', (req, res) => {
-	res.status(200).json(flipACoin("heads"))
+    res.send(flipACoin('heads'));
 });
+
 
 app.get('/app/flip/call/tails', (req, res) => {
-	res.status(200).json(flipACoin("tails"))
+    res.send(flipACoin('tails'));
 });
 
+
 // Default response for any other request
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404).send('404 NOT FOUND')
 });
+
+  
