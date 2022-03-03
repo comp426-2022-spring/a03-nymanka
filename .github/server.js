6f291c553727cf65175d899cc37e1f46293b96c5
Kaan Nyman <nymanka@ad.unc.edu>
2022-03-02 20:48:15 -0500

    oofg

--- .github/server.js
+++ .github/server.js
@@ -1,60 +1,116 @@
-//require dependencies
-import * as coin from "../a02-nymanka/modules/coin.mjs"
-import express from 'express';
-import Yargs from 'yargs';
-
-const app = express();
-const argv = Yargs(process.argv.slice(2)).argv;
-
-//define port as default or from cli
-const port = (argv.port == undefined) ? 5000 : argv.port;
-
-
-
 // Require Express.js
 const express = require('express')
 const app = express()
+const args = require('minimist')(process.argv.slice(2))
+
+args['port']
+
+const call = args.call
+const port = args.port || process.env.PORT || 5000
+
 
 // Start an app server
-const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
+
+// Define check endpoint
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+    res.statusCode = 200;
+
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage);
+});
+
+
+function coinFlip() {
+    return (Math.random() > 0.5 ? "heads" : "tails");
+  }
+
+
+app.get('/app/flip/', (req, res) => {
+    res.send({ flip: coinFlip() });
+});
+
+
+function coinFlips(flips) {
+    const coin = [];
+  
+    for(let i = 0; i < flips; i++){
+      Math.random() > 0.5 ? coin.push("heads") : coin.push("tails");
+    }
+  
+    const output = { raw: [], summary: "" };
+    output.raw = flipResults;
+    output.summary = countFlips(flipResults);
+    return coin;
+  }
+
+function countFlips(array) {
+    var heads = 0;
+    var tails = 0;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] == 'heads'){
+        heads++;
+      }
+      else{
+        tails++;
+      }
+    }
+    const count = new Map();
+  
+    count.set("tails", tails);
+    count.set("heads", heads);
+    
+    return count;
+  
+  }
+
+
+app.get('/app/flips/:number', (req, res) => {
+    res.send(coinFlips(req.params.number));
+});
+
+
+function flipACoin(call) {
+    if (call != "heads" & call != "tails" | call == null) {
+      console.log("Error: no input.")
+      console.log("Usage: node guess-flip --call=[heads|tails]")
+    }else{
+      const flip = coinFlip();
+      var result = "lose";
+      if(call == flip){
+        result = "win";
+    }
+    const count = new Map();
+  
+    count.set("call", call);
+    count.set("flip", flip);
+    count.set("result", result);
+   
+    return count;
+  
+    }
+}
+
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.send(flipACoin('heads'));
+});
+
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.send(flipACoin('tails'));
+});
+
+
 // Default response for any other request
-app.use(function(req, res){
+app.use(function (req, res) {
     res.status(404).send('404 NOT FOUND')
 });
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
-
-
-//random coin flip endpoint
-app.get('/app/flip', (req, res) => {
-    res.contentType('text/json');
-    res.status(200).json({ 'flip' : coin.coinFlip()});
-});
-
-app.get('/app/flips/:number', (req, res) => {
-    res.contentType('text/json');
-    const flips = coin.coinFlips(req.params.number);
-    const count = coin.countFlips(flips);
-    res.status(200).json({'Flips': flips,'info': count});
-});
-
-app.get('/app/flip/call/heads', (req, res) => {
-    res.contentType('text/json');
-    res.status(200).json(coin.coinFlip('heads'));
-});
-
-app.get('/app/flip/call/tails', (req, res) => {
-    res.contentType('text/json');
-    res.status(200).json(coin.coinFlip('tails'));
-});
-
+  
